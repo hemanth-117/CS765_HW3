945caf50ab5dae4b5dc1b8004defdff03ad6b412
hemanth210050117 <210050117@iitb.ac.in>
2024-04-15 00:21:57 +0530

    corrected simulation

--- simulation.py
+++ simulation.py
@@ -20,7 +20,7 @@ class voter():
         elif self.is_honest == 1:
             vote = np.random.choice([0, 1], p=[0.1, 0.9])
 
-        return [news_id, self.id, vote]
+        return [news_id, self.id, vote, self.trust]
     
     def get_trust(self):
         # return trust of voter
@@ -43,9 +43,8 @@ class voter():
             self.trust = 0
 
 class news():
-    def __init__(self, id, is_fake):
+    def __init__(self, id):
         self.id = id
-        self.is_fake = is_fake
         self.votes = []
 
     def election(self, voters):
@@ -53,12 +52,23 @@ class news():
             vote = peer.vote(self.id)
             self.votes.append(vote)
 
-    def fact_check(self, voters):
+    def get_rating(self):
+        # return rating of news
+        # rating is number of votes for news
+        rating = float(0);
+        totat_trust = float(0);
+        for vote in self.votes:
+            rating += vote[2]*vote[3]
+            totat_trust += vote[3]
+        
+        return rating/totat_trust > 0.5
+
+    def fact_check(self, voters, result):
         # increase trust of voters who voted news correctly
         # decrease trust of voters who voted news incorrectly
         for i, peer_vote in enumerate(self.votes):
             peer = voters[i]
-            if peer_vote[2] == self.is_fake:
+            if peer_vote[2] == result:
                 peer.inc_trust()
             else:
                 peer.dec_trust()
@@ -67,18 +77,20 @@ class news():
 N = int(input("Enter number of voters: "))
 
 # q is fraction of malicious voters
-q = float(input("Enter fraction of malicious voters: "))
+q = float(input("Enter fraction of malicious voters(q): "))
 
 # p is fraction of voters having more probability of voting correctly among 1-q
-p = float(input("Enter fraction of voters having more probability of voting correctly: "))
+p = float(input("Enter fraction of voters having more probability of voting correctly(p): "))
 
 # finally N*q are 0, N*(1-q)*p are 1, and N*(1-q)*(1-p) are 2
 voters = []
+mallecious = int(N * q)
+honest = int(N * (1 - q) * p)
 for i in range(N):
     # create voters
-    if i < N * q:
+    if i < mallecious:
         voters.append(voter(i, 0))
-    elif i < N * (1 - q) * p:
+    elif i < mallecious + honest:
         voters.append(voter(i, 1))
     else:
         voters.append(voter(i, 2))
@@ -91,30 +103,39 @@ M = int(input("Enter number of news: "))
 
 for i in range(M):
     # all news are true
-    current_news = news(i, 1)
+    current_news = news(i)
     # conduct election
     current_news.election(voters)
+    # get rating of news
+    rating = current_news.get_rating()
+    print("Rating of news", i, "is", rating)
     # fact check
-    current_news.fact_check(voters)
+    current_news.fact_check(voters, rating)
 # calculate average trust of voters
-avaerage_history = [0 for i in range(M)]
+avaerage_history_honest = [0 for i in range(M)]
+avaerage_history_mallicious = [0 for i in range(M)]
+avaerage_history_semi_honest = [0 for i in range(M)]
 for i in range(N):
-    for j in range(M):
-        avaerage_history[j] += voters[i].trust_history[j]
+    if voters[i].is_honest == 0:
+        for j in range(M):
+            avaerage_history_mallicious[j] += voters[i].trust_history[j]
+    elif voters[i].is_honest == 1:
+        for j in range(M):
+            avaerage_history_honest[j] += voters[i].trust_history[j]
+    else:
+        for j in range(M):
+            avaerage_history_semi_honest[j] += voters[i].trust_history[j]
 
-avaerage_history = [x / N for x in avaerage_history]
-    
-# plot trust history of voters
-for i in range(N):
-    # set x-axis and y-axis labels
-    s = "mallicious" if voters[i].is_honest == 0 else "honest" if voters[i].is_honest == 1 else "semi-honest"
-    plt.xlabel('news_id')
-    plt.ylabel('Trust')
-    plt.title('Trust History of Voter ' + str(i)+" " + s)
-    # make a gunplot of dots for trust_history
-    plt.plot(range(0, M), voters[i].trust_history, 'ro')
-    # make a gunplot of dots for average_history
-    plt.plot(range(0, M), avaerage_history, 'bo')
-    plt.legend(['Voter Trust', 'Average Trust'])
-    plt.savefig('trust_history_' + str(i) + '.png')
-    plt.clf()
+avaerage_history_honest = [x /honest for x in avaerage_history_honest]
+avaerage_history_semi_honest = [x / (N - honest - mallecious) for x in avaerage_history_semi_honest]
+avaerage_history_mallicious = [x / (mallecious) for x in avaerage_history_mallicious]
+
+# plot average trust of voters
+plt.plot(avaerage_history_honest, label='Honest')
+plt.plot(avaerage_history_semi_honest, label='Semi-Honest')
+plt.plot(avaerage_history_mallicious, label='Malicious')
+plt.xlabel('News')
+plt.ylabel('Average Trust')
+plt.legend()
+plt.savefig('trust.png')
+plt.show()
